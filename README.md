6aac312ad8ba70585b1dc8a4adaeb6f9a0a5a9a3
Nice Guy <tabvim.dev@gmail.com>
2022-12-06 16:55:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 安装whisper
+
+```bash
+pip install git+https://github.com/openai/whisper.git 
+```
---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, flash, request
+from werkzeug.utils import secure_filename
+import os
+
+import whisper
+
+
+def prediction(audio):
+    print(audio)
+    model = whisper.load_model("base")
+    result = model.transcribe(audio)
+    return result
+
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['UPLOAD_FOLDER'] = './tmp'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+
+@app.route('/ping')
+def hello_world():
+    return 'pong'
+
+
+ALLOWED_EXTENSIONS = {'m4a', 'mp3', 'wav', 'aifc', 'aiff', 'mp2', 'ogg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return {
+                'success': False
+            }
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return {
+                'success': False
+            }
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('download_file', name=filename))
+            prections = prediction(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
+            return {
+                'success': False,
+                'data': prections
+            }
+
+    return {
+        'success': False
+    }
