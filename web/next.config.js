21a4445690c602a530c8dd2eddd3a57d5442f053
Nice Guy <tabvim.dev@gmail.com>
2022-12-07 17:21:49 +0800

    更新

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,25 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/dist/shared/lib/constants')
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8081/:path*'
+module.exports = (phase, { defaultConfig }) => {
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      reactStrictMode: true,
+      async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://127.0.0.1:8081/:path*'
+          }
+        ]
       }
-    ]
+    }
+  }
+
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
   }
 }
