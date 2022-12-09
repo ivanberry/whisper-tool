034e46881e9a9baaa0df7a62f394fbad9fc23475
Nice Guy <tabvim.dev@gmail.com>
2022-12-09 11:02:29 +0800

    update

--- web/pages/api/auth/[...nextauth].js
+++ web/pages/api/auth/[...nextauth].js
@@ -11,7 +11,8 @@ export const authOptions = {
     }),
     GitHubProvider({
       clientId: 'Iv1.f55eacb2a7a0e634',
-      clientSecret: '8a3d6064dc902167921ce692a259f2a1246fcd88'
+      clientSecret: '8a3d6064dc902167921ce692a259f2a1246fcd88',
+      checks: 'none'
     })
   ],
   debug: true,
